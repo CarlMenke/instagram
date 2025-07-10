e6d1e5e9746c3360c641e9d1790fe965b14f4780
JulianC775 <juliancatello4@gmail.com>
2025-07-10 15:10:58 -0700

    Add root, sidebar, and post styles; implement Post component in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { SideBar } from './components/SideBar/SideBar'
+import { Post } from './components/MainFeed/Post'
+import { User } from "lucide-react"
 
 function App() {
 
   return (
-    <div style={{height: '100vh', background: 'black'}}>
+    <div className='rootContainer'>
       <SideBar logotext="Instagram" />
+      <Post profileIcon={<User/>} profileUsername="UserName" postPhoto="https://upload.wikimedia.org/wikipedia/commons/8/8e/Rubber_duckies_So_many_ducks.jpg" postComments="Comments"/>
     </div>
   );
 }
--- src/components/MainFeed/Post.jsx
+++ src/components/MainFeed/Post.jsx
@@ -1,5 +1,24 @@
-picture
-caption
-username
+import React from "react";  
+/* profile logo
+    username
+    photo
+    caption
+ */ 
 
 
+export const Post = (props) => {
+
+    return (
+        <div className='postContainer'> 
+            <div className="postHeaderContainer">
+                <div className='profileIcon'>{props.profileIcon}</div>
+                <div className='profileUsername'>{props.profileUsername}</div>
+            </div>
+            
+            <img className='postPhoto' src={props.postPhoto} width="270px"/>
+            <div className='postComments'>{props.postComments}</div>
+        </div>
+            
+    )
+
+} 
